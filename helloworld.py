b570a42ec88be24b8a5d8d98f7c9f25ec3b652cf
cyumair <47939238+cyumair@users.noreply.github.com>
2020-01-03 20:01:50 +0500

    Update and rename helloworld.py to 1.Hello_World.py

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-print('This program Calculates Factorial of a Number')
-def factorial(n):
-    if n==0:
-        return 1
-    else:
-        return factorial(n-1)*n
-while True:
-    n = int(input('please enter any number ...'))
-    print('factorial of number is:', factorial(n))    
